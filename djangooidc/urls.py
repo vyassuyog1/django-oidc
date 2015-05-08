8a3d574f681d9d5d913173e0cb76ba76d5b3ce5f
marcanpilami <marsu_pilami@msn.com>
2015-05-08 16:16:10 +0200

    login URL should be login...

--- djangooidc/urls.py
+++ djangooidc/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = patterns('',
-                       url(r'^openid$', views.openid, name='openid'),
+                       url(r'^login$', views.openid, name='openid'),
                        url(r'^openid/(?P<op_name>.+)$', views.openid, name='openid_with_op_name'),
                        url(r'^callback/?', views.authz_cb, name='openid_cb'),
                        url(r'^logout$', views.logout, name='logout'),
