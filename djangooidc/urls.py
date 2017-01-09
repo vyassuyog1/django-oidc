930e19d982ca87cff20e5c574e6b91483462e515
Derek Pryor <Derek.Pryor@jhuapl.edu>
2017-01-09 08:33:58 -0800

    Changed urlpatterns to no longer use django.conf.urls.patterns (deprecated)

--- djangooidc/urls.py
+++ djangooidc/urls.py
@@ -1,12 +1,12 @@
 # coding: utf-8
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from . import views
 
-urlpatterns = patterns('',
-                       url(r'^login$', views.openid, name='openid'),
-                       url(r'^openid/(?P<op_name>.+)$', views.openid, name='openid_with_op_name'),
-                       url(r'^callback/login/?$', views.authz_cb, name='openid_login_cb'),
-                       url(r'^logout$', views.logout, name='logout'),
-                       url(r'^callback/logout/?$', views.logout_cb, name='openid_logout_cb'),
-                       )
+urlpatterns = [
+    url(r'^login$', views.openid, name='openid'),
+    url(r'^openid/(?P<op_name>.+)$', views.openid, name='openid_with_op_name'),
+    url(r'^callback/login/?$', views.authz_cb, name='openid_login_cb'),
+    url(r'^logout$', views.logout, name='logout'),
+    url(r'^callback/logout/?$', views.logout_cb, name='openid_logout_cb'),
+]
